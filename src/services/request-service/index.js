dc0f30a960b1e11fc68b10f6c3367a418649bd81
Harutyun Mkhitaryan <harutyun.mkhitaryan@softconstruct.com>
2021-06-11 15:35:34 +0400

    fixes in request-service

--- src/services/request-service/index.js
+++ src/services/request-service/index.js
@@ -2,89 +2,87 @@ import axios from 'axios';
 import qs from 'qs';
 
 class RequestService {
-    constructor() {
-        this.source = null;
-    }
+	constructor() {
+		this.source = null;
+	}
 
-    get = (url, options) => {
-        options = options || {};
-        return this.makeAPIRequest(url, options, 'GET');
-    };
+	get = (url, options) => {
+		options = options || {};
+		return this.makeAPIRequest(url, options, 'GET');
+	};
 
-    post = (url, options) => {
-        options = options || {};
-        return this.makeAPIRequest(url, options, 'POST');
-    };
+	post = (url, options) => {
+		options = options || {};
+		return this.makeAPIRequest(url, options, 'POST');
+	};
 
-    put = (url, options) => {
-        options = options || {};
-        return this.makeAPIRequest(url, options, 'PUT');
-    };
+	put = (url, options) => {
+		options = options || {};
+		return this.makeAPIRequest(url, options, 'PUT');
+	};
 
-    delete = (url, options) => {
-        options = options || {};
-        return this.makeAPIRequest(url, options, 'DELETE');
-    };
+	delete = (url, options) => {
+		options = options || {};
+		return this.makeAPIRequest(url, options, 'DELETE');
+	};
 
-    patch = (url, options) => {
-        options = options || {};
-        return this.makeAPIRequest(url, options, 'PATCH');
-    };
+	patch = (url, options) => {
+		options = options || {};
+		return this.makeAPIRequest(url, options, 'PATCH');
+	};
 
-    makeAPIRequest = (url, options = {}, method) => {
-        return new Promise((resolve, reject) => {
+	makeAPIRequest = (url, options = {}, method) => {
+		return new Promise((resolve, reject) => {
+			if (this.source) {
+				this.source.cancel();
+			}
 
-            if (this.source) {
-                this.source.cancel();
-            }
+			if (!url) {
+				return reject('INVALID_REQUEST_PARAMS');
+			}
 
-            if (!url) {
-                return reject('INVALID_REQUEST_PARAMS');
-            }
+			this.source = axios.CancelToken.source();
 
-            this.source = axios.CancelToken.source();
+			let fetch_options = {
+				url,
+				method,
+				headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					'Access-Control-Allow-Methods': '*',
+					version: 1,
+					...options,
+				},
+				cancelToken: this.source.token,
+				data: options.body || {},
+				params: options.queryParams || '',
+				paramsSerializer: (params) => qs.stringify(params),
+				timeout: options?.timeout ?? 10000,
+			};
 
-            let fetch_options = {
-                url,
-                method,
-                headers: {
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                    'Access-Control-Allow-Methods': "*",
-                    'version': 1,
-                    ...options
-                },
-                cancelToken: this.source.token,
-                data: options.body || {},
-                params: options.queryParams || '',
-                paramsSerializer: params => qs.stringify(params),
-                timeout: options?.timeout ?? 10000
-            };
+			if (options.headers) {
+				fetch_options.headers = options.headers;
+			}
 
-            if (options.headers) {
-                fetch_options.headers = options.headers;
-            }
+			axios(fetch_options)
+				.then(async (response) => {
+					if (!response) {
+						return reject('INVALID_RESPONSE_DATA');
+					}
 
-            axios(fetch_options)
-                .then(async response => {
+					let { data } = response;
 
-                    if (!response) {
-                        return reject('INVALID_RESPONSE_DATA');
-                    }
-
-                    let { data } = response;
-
-                    return resolve(data);
-                }).catch(err => {
-                    if (err.message !== "The user aborted a request.") {
-                        err = JSON.parse(JSON.stringify(err));
-                        // let check_result = await this._validation_service.validate(data);
-                        return reject(err);
-                    }
-                });
-        });
-    }
+					return resolve(data);
+				})
+				.catch((err) => {
+					err = JSON.parse(JSON.stringify(err));
+					if (err.message !== 'The user aborted a request.') {
+						// let check_result = await this._validation_service.validate(data);
+						return reject(err);
+					}
+				});
+		});
+	};
 }
 
-
-export default new RequestService;
+export default new RequestService();
