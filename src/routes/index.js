dacf9a8d1dc1043affcaa58e3670d3eba360cbc3
Harutyun Mkhitaryan <harutyun.mkhitaryan@softconstruct.com>
2021-06-11 14:41:08 +0400

    fixes in jsconfig

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { createBrowserHistory } from "history";
-import { Redirect, Route, Switch, Router } from 'react-router-dom';
-import SignIn from '../pages/sign-in';
+import { createBrowserHistory } from 'history';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import SignIn from 'pages/sign-in';
 
 const history = createBrowserHistory();
 
 const Routes = () => {
-
-    return (
-        <Router history={history}>
-            <Switch>
-                <Route path="/" exact component={() => <div>asssssssssssssssssssss</div>} />
-                <Route path="/sign-in" component={SignIn} />
-                <Route exact path="/no-match" component={() => <h1>404</h1>} />
-                <Redirect to="/no-match" />
-            </Switch>
-        </Router>
-    );
+	return (
+		<Router history={history}>
+			<Switch>
+				<Redirect exact from="/" to="/sign-in" />
+				<Route exact path="/sign-in" component={SignIn} />
+				<Route exact path="/404" component={() => <div>no match</div>} />
+				<Redirect exact to="/404" />
+				{/*<Route path="*"  component={() => <div>no match</div>} />*/}
+			</Switch>
+		</Router>
+	);
 };
 
 export default Routes;
